7da52ba9895a48f23a0dd2424b5f121f86945cfb
huting <804427626@qq.com>
2020-04-02 14:38:55 +0800

    使用LogBack日志组件

--- src/main/java/com/demo/controller/HelloController.java
+++ src/main/java/com/demo/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloController {
+
+    /**
+     * logback
+     */
+    private final static Logger logger = LoggerFactory.getLogger(HelloController.class);
+
     @RequestMapping(value = "hello")
     public String hello() {
+        logger.info("访问了hello方法");
+        logger.debug("记录debug日志");
+        logger.error("记录了error错误日志");
         return "Hello Word 之 Spring boot";
     }
 }
